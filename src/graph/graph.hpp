f9feaf780e0580c6f88d5860c195181086d787a0
sfod <octosyllabic@gmail.com>
2015-10-27 18:46:21 +0300

    Implement Graph methods within class definition

--- src/graph/graph.hpp
+++ src/graph/graph.hpp
@@ -7,60 +7,36 @@
 template<template<bool, typename> class GraphImp, bool Dir, typename T>
 class Graph {
 public:
-    Graph();
+    Graph() : graph_() {
+        static_assert(std::is_base_of<GraphI<T>, GraphImp<Dir, T>>::value, "GraphImp must implement GraphI");
+    }
+
     virtual ~Graph() = default;
 
-    int add_vertex(const Vertex<T> &vertex);
-    void rm_vertex(const Vertex<T> &vertex);
-    const Vertex<T> &vertex(int idx) const;
-    int vertex_num() const;
+    int add_vertex(const Vertex<T> &vertex) {
+        return graph_.add_vertex(vertex);
+    }
 
-    int add_edge(int idx1, int idx2);
-    virtual bool find_path(int start_idx, int end_idx, std::vector<int> *path) const;
+    void rm_vertex(const Vertex<T> &vertex) {
+        graph_.rm_vertex(vertex);
+    }
+
+    const Vertex<T> &vertex(int idx) const {
+        return graph_.vertex(idx);
+    }
+
+    int vertex_num() const {
+        return graph_.vertex_num();
+    }
+
+    int add_edge(int idx1, int idx2) {
+        return graph_.add_edge(idx1, idx2);
+    }
+
+    virtual bool find_path(int start_idx, int end_idx, std::vector<int> *path) const {
+        return graph_.find_path(start_idx, end_idx, path);
+    }
 
 private:
     GraphImp<Dir, T> graph_;
 };
-
-template<template<bool, typename> class GraphImp, bool Dir, typename T>
-Graph<GraphImp, Dir, T>::Graph() : graph_()
-{
-    static_assert(std::is_base_of<GraphI<T>, GraphImp<Dir, T>>::value, "GraphImp must implement GraphI");
-}
-
-template<template<bool, typename> class GraphImp, bool Dir, typename T>
-int Graph<GraphImp, Dir, T>::add_vertex(const Vertex<T> &vertex)
-{
-    return graph_.add_vertex(vertex);
-}
-
-template<template<bool, typename> class GraphImp, bool Dir, typename T>
-void Graph<GraphImp, Dir, T>::rm_vertex(const Vertex<T> &vertex)
-{
-    graph_.rm_vertex(vertex);
-}
-
-template<template<bool, typename> class GraphImp, bool Dir, typename T>
-const Vertex<T> &Graph<GraphImp, Dir, T>::vertex(int idx) const
-{
-    return graph_.vertex(idx);
-}
-
-template<template<bool, typename> class GraphImp, bool Dir, typename T>
-int Graph<GraphImp, Dir, T>::vertex_num() const
-{
-    return graph_.vertex_num();
-}
-
-template<template<bool, typename> class GraphImp, bool Dir, typename T>
-int Graph<GraphImp, Dir, T>::add_edge(int idx1, int idx2)
-{
-    graph_.add_edge(idx1, idx2);
-    return 0;
-}
-
-template<template<bool, typename> class GraphImp, bool Dir, typename T>
-bool Graph<GraphImp, Dir, T>::find_path(int start_idx, int end_idx, std::vector<int> *path) const
-{
-    return graph_.find_path(start_idx, end_idx, path);
-}
