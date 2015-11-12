173ae23ba3c4c7fab522a83da9cbdb31dfa45cd2
sfod <octosyllabic@gmail.com>
2015-11-12 12:58:37 +0300

    Add BFS tests

--- test/test_list_graph.cpp
+++ test/test_list_graph.cpp
@@ -29,6 +29,44 @@ TEST_F(ListGraphUndirectedTest, test_set_vertex)
     EXPECT_EQ(5, g.vertex(1).data());
 }
 
+TEST_F(ListGraphUndirectedTest, test_bfs_direct_order)
+{
+    for (int i = 0; i < 4; ++i) {
+        g.set_vertex(Vertex<int>(i, i));
+    }
+
+    EXPECT_EQ(4, g.vertex_num());
+
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+
+    std::vector<int> path;
+    int p = 3;
+    std::function<bool(int)> f = [&](int c) { return c == p; };
+    EXPECT_EQ(true, g.bfs(0, f, &path));
+}
+
+TEST_F(ListGraphUndirectedTest, test_bfs_reverse_order)
+{
+    for (int i = 0; i < 4; ++i) {
+        g.set_vertex(Vertex<int>(i, i));
+    }
+
+    EXPECT_EQ(4, g.vertex_num());
+
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+
+    std::vector<int> path;
+    int p = 0;
+    std::function<bool(int)> f = [&](int c) { return c == p; };
+    EXPECT_EQ(true, g.bfs(3, f, &path));
+}
+
+}  // namespace
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
