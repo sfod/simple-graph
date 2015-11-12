4c75b85898eba01e9dc986eabe4982a5c3043630
sfod <octosyllabic@gmail.com>
2015-11-12 12:58:11 +0300

    Fix tests: store graph in fixture class

--- test/test_list_graph.cpp
+++ test/test_list_graph.cpp
@@ -3,30 +3,21 @@
 
 namespace {
 
-class ListGraphTest : public ::testing::Test {
+class ListGraphUndirectedTest : public ::testing::Test {
 protected:
-    ListGraphTest() {}
-    virtual ~ListGraphTest() {}
-    virtual void SetUp() {}
-    virtual void TearDown() {}
+    ListGraph<false, int> g;
 };
 
-}
-
-TEST_F(ListGraphTest, add_vertex)
+TEST_F(ListGraphUndirectedTest, test_add_vertex)
 {
-    ListGraph<false, int> g;
     EXPECT_EQ(0, g.vertex_num());
-
     g.add_vertex(Vertex<int>(2));
     EXPECT_EQ(3, g.vertex_num());
-
     EXPECT_EQ(-1, g.add_vertex(Vertex<int>(2)));
 }
 
-TEST_F(ListGraphTest, set_vertex)
+TEST_F(ListGraphUndirectedTest, test_set_vertex)
 {
-    ListGraph<false, int> g;
     EXPECT_EQ(0, g.vertex_num());
 
     g.set_vertex(Vertex<int>(1, 4));
