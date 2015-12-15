09f7cd3d242c21626a1135cefa0d5045d6c8c0a1
sfod <octosyllabic@gmail.com>
2015-12-15 18:20:16 +0300

    Remove matrix graph from library target

--- test/main.cpp
+++ test/main.cpp
@@ -1,28 +1,9 @@
 #include <iostream>
 #include "graph/graph.hpp"
 #include "graph/list_graph.hpp"
-#include "graph/matrix_graph.hpp"
 
 int main()
 {
-    Graph<MatrixGraph, true, std::string, int> mg;
-    mg.add_vertex(Vertex<std::string>(0, "zero"));
-    mg.add_vertex(Vertex<std::string>(1, "one"));
-    mg.add_vertex(Vertex<std::string>(2, "two"));
-    mg.add_edge(Edge<int>(0, 1));
-    mg.add_edge(Edge<int>(1, 2));
-    std::vector<int> mpath;
-    std::string ms = "two";
-    std::function<bool(std::string)> mf = [&] (const std::string &d) { return d == ms; };
-    if (mg.find_vertex(0, mf, &mpath)) {
-        for (auto n : mpath) {
-            std::cout << mg.vertex(n).data() << std::endl;
-        }
-    }
-    else {
-        std::cout << "no path" << std::endl;
-    }
-
     Graph<ListGraph, false, std::string, int> lg;
     lg.add_vertex(Vertex<std::string>(0, "zero"));
     lg.add_vertex(Vertex<std::string>(1, "one"));
