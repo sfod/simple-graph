449395cc12348e547d96a0bf930a8c0a57ea318f
sfod <octosyllabic@gmail.com>
2016-03-16 18:12:03 +0300

    Optimize Bellman-Ford algorithm

--- src/simple_graph/bellman_ford.hpp
+++ src/simple_graph/bellman_ford.hpp
@@ -15,11 +15,16 @@ bool bellman_ford(const Graph<V, E> &g, size_t start_idx, size_t goal_idx, std::
 
     Graph<V, E> *fg = const_cast<Graph<V, E>*>(&g);
     for (size_t i = 0; i < g.vertex_num(); ++i) {
+        bool changed = false;
         for (auto e : fg->edges()) {
             if (distance[e.idx1()] + e.weight() < distance[e.idx2()]) {
                 distance[e.idx2()] = distance[e.idx1()] + e.weight();
                 predecessor[e.idx2()] = e.idx1();
+                changed = true;
             }
+        }
+        if (!changed) {
+            break;
         }
     }
 
