23b5d55dbc01b572f174c056d5db58bfbb9920b6
sfod <octosyllabic@gmail.com>
2016-03-25 18:01:23 +0300

    Fix performance tests

--- test/test_performance.cpp
+++ test/test_performance.cpp
@@ -1,8 +1,9 @@
 #include <random>
 #include "benchmark/benchmark.h"
 #include "simple_graph/list_graph.hpp"
+#include "simple_graph/bfs.hpp"
 
-static void BM_Construction(benchmark::State &state)
+static void bench_creation(benchmark::State &state)
 {
     simple_graph::ListGraph<false, int, int> g;
     while (state.KeepRunning()) {
@@ -10,21 +11,39 @@ static void BM_Construction(benchmark::State &state)
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<> dist(0, state.range_x() - 1);
-        std::set<std::pair<int, int>> data;
+        std::set<std::pair<size_t, size_t>> data;
         for (int j = 0; j < state.range_y(); ++j) {
             data.insert(std::make_pair<int, int>(dist(gen), dist(gen)));
         }
         state.ResumeTiming();
 
         for (int i = 0; i < state.range_x(); ++i) {
-            g.add_vertex(simple_graph::Vertex<int>(i));
+            g.add_vertex(simple_graph::Vertex<int>(static_cast<size_t>(i)));
         }
         for (auto &d : data) {
             g.add_edge(simple_graph::Edge<int>(d.first, d.second));
         }
     }
 }
-BENCHMARK(BM_Construction)->RangePair(1<<10, 8<<16, 100, 1<<12);
+BENCHMARK(bench_creation)->RangePair(1<<10, 8<<16, 100, 1<<12);
+
+static void bench_bfs_path_length(benchmark::State &state)
+{
+    simple_graph::ListGraph<false, int, ssize_t> g;
+    for (int i = 0; i < state.range_x(); ++i) {
+        g.set_vertex(simple_graph::Vertex<int>(i));
+    }
+    for (int i = 0; i < state.range_x() - 1; ++i) {
+        g.add_edge(simple_graph::Edge<ssize_t>(i, i + 1, 1));
+    }
+
+    std::function<bool(int)> f = [&](int c) { return c == state.range_x() - 1; };
+
+    while (state.KeepRunning()) {
+        std::vector<size_t> path;
+        benchmark::DoNotOptimize(simple_graph::bfs(g, 0, f, &path));
+    }
+}
+BENCHMARK(bench_bfs_path_length)->Range(1<<10, 1<<16);
 
 BENCHMARK_MAIN()
-
