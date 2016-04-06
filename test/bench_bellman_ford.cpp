41d2e1461fdd5ffd955ec56c89aa6448296bae9a
sfod <octosyllabic@gmail.com>
2016-04-06 23:11:21 +0300

    Use google benchmark in Bellman-Ford benchmarking

---
+++ test/bench_bellman_ford.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "benchmark/benchmark.h"
+#include "simple_graph/list_graph.hpp"
+#include "simple_graph/bellman_ford.hpp"
+
+using simple_graph::vertex_index_t;
+
+static void bench_bellman_ford(benchmark::State &state)
+{
+    simple_graph::ListGraph<true, int, ssize_t> g;
+    for (int i = 0; i < state.range_x(); ++i) {
+        g.add_vertex(simple_graph::Vertex<int>(i, i));
+    }
+    for (int i = 0; i < state.range_x() - 1; ++i) {
+        g.add_edge(simple_graph::Edge<ssize_t>(i, i + 1, 1));
+    }
+
+    while (state.KeepRunning()) {
+        std::vector<vertex_index_t> path;
+        benchmark::DoNotOptimize(simple_graph::bellman_ford(g, 0, state.range_x() - 1, &path));
+    }
+}
+BENCHMARK(bench_bellman_ford)->Arg(1<<15);
+
+static void bench_bellman_ford_reversed(benchmark::State &state)
+{
+    simple_graph::ListGraph<true, int, ssize_t> g;
+    for (int i = 0; i < state.range_x(); ++i) {
+        g.add_vertex(simple_graph::Vertex<int>(i, i));
+    }
+    for (int i = 0; i < state.range_x() - 1; ++i) {
+        g.add_edge(simple_graph::Edge<ssize_t>(i + 1, i, 1));
+    }
+
+    while (state.KeepRunning()) {
+        std::vector<vertex_index_t> path;
+        benchmark::DoNotOptimize(simple_graph::bellman_ford(g, state.range_x() - 1, 0, &path));
+    }
+}
+BENCHMARK(bench_bellman_ford_reversed)->Range(1<<8, 1<<10);
+
+BENCHMARK_MAIN()
