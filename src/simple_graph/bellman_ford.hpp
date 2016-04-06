5ed8617afc213150f04345ded71671517c17d6f9
sfod <octosyllabic@gmail.com>
2016-04-06 17:00:27 +0300

    Fix Bellman-Ford algorithm

--- src/simple_graph/bellman_ford.hpp
+++ src/simple_graph/bellman_ford.hpp
@@ -5,12 +5,26 @@
 
 namespace simple_graph {
 
+template<typename E>
+bool check_distance(const E &d, const E &w)
+{
+    if ((d > 0) && (w >0) && (std::numeric_limits<E>::max() - d < w)) {
+        return false;
+    }
+    return true;
+}
+
 template<typename V, typename E>
 bool bellman_ford(const Graph<V, E> &g, vertex_index_t start_idx, vertex_index_t goal_idx,
         std::vector<vertex_index_t> *path)
 {
+    // TODO add utility function to check passed vertex indices
+    if ((start_idx < 0) || (goal_idx < 0)) {
+        return false;
+    }
+
     std::vector<E> distance(g.vertex_num(), std::numeric_limits<E>::max());
-    std::vector<vertex_index_t> predecessor(g.vertex_num(), 0);
+    std::vector<vertex_index_t> predecessor(g.vertex_num(), -1);
 
     distance[start_idx] = 0;
 
@@ -18,7 +32,8 @@ bool bellman_ford(const Graph<V, E> &g, vertex_index_t start_idx, vertex_index_t
     for (size_t i = 0; i < g.vertex_num(); ++i) {
         bool changed = false;
         for (auto e : fg->edges()) {
-            if (distance[e.idx1()] + e.weight() < distance[e.idx2()]) {
+            if (check_distance(distance[e.idx1()], e.weight())
+                    && (distance[e.idx1()] + e.weight() < distance[e.idx2()])) {
                 distance[e.idx2()] = distance[e.idx1()] + e.weight();
                 predecessor[e.idx2()] = e.idx1();
                 changed = true;
@@ -30,12 +45,13 @@ bool bellman_ford(const Graph<V, E> &g, vertex_index_t start_idx, vertex_index_t
     }
 
     for (auto e : fg->edges()) {
-        if (distance[e.idx1()] + e.weight() < distance[e.idx2()]) {
+        if (check_distance(distance[e.idx1()], e.weight())
+                && (distance[e.idx1()] + e.weight() < distance[e.idx2()])) {
             return false;
         }
     }
 
-    if (distance[goal_idx] == 0) {
+    if (distance[goal_idx] == std::numeric_limits<E>::max()) {
         return false;
     }
 
@@ -43,6 +59,7 @@ bool bellman_ford(const Graph<V, E> &g, vertex_index_t start_idx, vertex_index_t
     while (idx != start_idx) {
         path->push_back(idx);
         idx = predecessor[idx];
+        assert(idx != -1);
     }
     path->push_back(idx);
     std::reverse(path->begin(), path->end());
