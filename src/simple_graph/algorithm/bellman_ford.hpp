81b38092c911f81e927a37f0d77edb90321a7e82
sfod <octosyllabic@gmail.com>
2016-04-13 17:37:33 +0300

    Optimize Bellman-Graph algorithm (Yen's improvement)
    Traverse all edges in arbitrary order.

--- src/simple_graph/algorithm/bellman_ford.hpp
+++ src/simple_graph/algorithm/bellman_ford.hpp
@@ -30,13 +30,51 @@ bool bellman_ford(const Graph<Dir, V, E> &g, vertex_index_t start_idx, vertex_in
     distance[start_idx] = 0;
 
     Graph<Dir, V, E> *fg = const_cast<Graph<Dir, V, E>*>(&g);
+
+    std::vector<Edge<E>> asc_edges;
+    std::vector<Edge<E>> desc_edges;
+    for (const auto &edge : fg->edges()) {
+        if (Dir) {
+            if (edge.idx1() < edge.idx2()) {
+                asc_edges.push_back(edge);
+            }
+            else {
+                desc_edges.push_back(edge);
+            }
+        }
+        else {
+            asc_edges.push_back(edge);
+            desc_edges.push_back(edge);
+        }
+    }
+    std::sort(asc_edges.begin(), asc_edges.end(), [](const Edge<E> &a, const Edge<E> &b) {
+        vertex_index_t a_idx = std::min(a.idx1(), a.idx2());
+        vertex_index_t b_idx = std::min(b.idx1(), b.idx2());
+        return a_idx < b_idx;
+    });
+    std::sort(desc_edges.begin(), desc_edges.end(), [](const Edge<E> &a, const Edge<E> &b) {
+        vertex_index_t a_idx = std::max(a.idx1(), a.idx2());
+        vertex_index_t b_idx = std::max(b.idx1(), b.idx2());
+        return b_idx < a_idx;
+    });
+
     for (size_t i = 0; i < g.vertex_num(); ++i) {
         bool changed = false;
-        for (const auto &edge : fg->edges()) {
-            if (check_distance(distance[edge.idx1()], edge.weight())
-                    && (distance[edge.idx1()] + edge.weight() < distance[edge.idx2()])) {
-                distance[edge.idx2()] = distance[edge.idx1()] + edge.weight();
-                predecessor[edge.idx2()] = edge.idx1();
+        for (const auto &edge : asc_edges) {
+            std::pair<const vertex_index_t &, const vertex_index_t &> e = std::minmax(edge.idx1(), edge.idx2());
+            if (check_distance(distance[e.first], edge.weight())
+                    && (distance[e.first] + edge.weight() < distance[e.second])) {
+                distance[e.second] = distance[e.first] + edge.weight();
+                predecessor[e.second] = e.first;
+                changed = true;
+            }
+        }
+        for (const auto &edge : desc_edges) {
+            std::pair<const vertex_index_t &, const vertex_index_t &> e = std::minmax(edge.idx1(), edge.idx2());
+            if (check_distance(distance[e.second], edge.weight())
+                    && (distance[e.second] + edge.weight() < distance[e.first])) {
+                distance[e.first] = distance[e.second] + edge.weight();
+                predecessor[e.first] = e.second;
                 changed = true;
             }
         }
