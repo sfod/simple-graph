3f6096caf287c065a36d8caff14406daa64af8cb
sfod <octosyllabic@gmail.com>
2016-04-17 15:38:16 +0300

    Add benchmark test for Bellman-Ford

--- test/bench_bellman_ford.cpp
+++ test/bench_bellman_ford.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include "benchmark/benchmark.h"
 #include "simple_graph/list_graph.hpp"
@@ -38,5 +39,25 @@ static void bench_bellman_ford_reversed(benchmark::State &state)
     }
 }
 BENCHMARK(bench_bellman_ford_reversed)->Range(1<<10, 1<<16);
+
+static void bench_bellman_ford_random(benchmark::State &state)
+{
+    std::vector<vertex_index_t> vertices;
+    simple_graph::ListGraph<true, int, ssize_t> g;
+    for (int i = 0; i < state.range_x(); ++i) {
+        g.add_vertex(simple_graph::Vertex<int>(i, i));
+        vertices.push_back(i);
+    }
+    std::random_shuffle(vertices.begin(), vertices.end());
+    for (int i = 0; i < state.range_x() - 1; ++i) {
+        g.add_edge(simple_graph::Edge<ssize_t>(vertices[i], vertices[i + 1], 1));
+    }
+
+    while (state.KeepRunning()) {
+        std::vector<vertex_index_t> path;
+        benchmark::DoNotOptimize(simple_graph::bellman_ford(g, vertices[0], vertices[state.range_x() - 1], &path));
+    }
+}
+BENCHMARK(bench_bellman_ford_random)->Range(1<<10, 1<<13);
 
 BENCHMARK_MAIN()
