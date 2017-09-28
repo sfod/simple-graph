ada3e4552b1d31e33b3ac633cec083ab8df8e4c1
sfod <octosyllabic@gmail.com>
2017-09-28 19:44:21 +0300

    Add A* tests for directed ListGraph

--- test/test_astar.cpp
+++ test/test_astar.cpp
@@ -7,9 +7,10 @@ namespace {
 
 using simple_graph::vertex_index_t;
 
-class UndirectedListGraphTest : public ::testing::Test {
+class ListGraphTest : public ::testing::Test {
 protected:
     simple_graph::ListGraph<false, std::pair<float, float>, float> undirected_graph;
+    simple_graph::ListGraph<true, std::pair<float, float>, float> directed_graph;
 };
 
 static float dist(const simple_graph::ListGraph<false, std::pair<float, float>, float> &g,
@@ -22,7 +23,17 @@ static float dist(const simple_graph::ListGraph<false, std::pair<float, float>, 
     return std::sqrt(std::pow(xdiff, 2) + std::pow(ydiff, 2));
 }
 
-TEST_F(UndirectedListGraphTest, test_astar_long)
+static float dist(const simple_graph::ListGraph<true, std::pair<float, float>, float> &g,
+        vertex_index_t c, vertex_index_t r)
+{
+    const auto &cdata = g.vertex(c).data();
+    const auto &rdata = g.vertex(r).data();
+    float xdiff = rdata.first - cdata.first;
+    float ydiff = rdata.second - cdata.second;
+    return std::sqrt(std::pow(xdiff, 2) + std::pow(ydiff, 2));
+}
+
+TEST_F(ListGraphTest, test_undirected_astar_long)
 {
     undirected_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(0, std::make_pair(0.0f, 0.0f)));
     undirected_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(1, std::make_pair(1.0f, 1.0f)));
@@ -55,7 +66,7 @@ TEST_F(UndirectedListGraphTest, test_astar_long)
     EXPECT_EQ(4, path[4]);
 }
 
-TEST_F(UndirectedListGraphTest, test_astar_short)
+TEST_F(ListGraphTest, test_undirected_astar_short)
 {
     undirected_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(0, std::make_pair(0.0f, 0.0f)));
     undirected_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(1, std::make_pair(1.0f, 1.0f)));
@@ -87,7 +98,7 @@ TEST_F(UndirectedListGraphTest, test_astar_short)
     EXPECT_EQ(6, path[2]);
 }
 
-TEST_F(UndirectedListGraphTest, test_astar_no_path)
+TEST_F(ListGraphTest, test_undirected_astar_no_path)
 {
     undirected_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(0, std::make_pair(0.0f, 0.0f)));
     undirected_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(1, std::make_pair(1.0f, 1.0f)));
@@ -105,6 +116,39 @@ TEST_F(UndirectedListGraphTest, test_astar_no_path)
     };
     EXPECT_EQ(false, astar(undirected_graph, 0, 3, heuristic, &path));
     EXPECT_EQ(0, path.size());
+}
+
+TEST_F(ListGraphTest, test_directed_astar_long)
+{
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(0, std::make_pair(0.0f, 0.0f)));
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(1, std::make_pair(1.0f, 1.0f)));
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(2, std::make_pair(2.0f, 2.0f)));
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(3, std::make_pair(5.0f, 5.0f)));
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(4, std::make_pair(7.5f, 7.5f)));
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(5, std::make_pair(3.0f, 1.0f)));
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(6, std::make_pair(10.0f, 1.0f)));
+    directed_graph.add_vertex(simple_graph::Vertex<std::pair<float, float>>(7, std::make_pair(10.0f, 10.0f)));
+
+    EXPECT_EQ(8, directed_graph.vertex_num());
+
+    directed_graph.add_edge(simple_graph::Edge<float>(0, 1, dist(directed_graph, 0, 1)));
+    directed_graph.add_edge(simple_graph::Edge<float>(1, 2, dist(directed_graph, 1, 2)));
+    directed_graph.add_edge(simple_graph::Edge<float>(2, 3, dist(directed_graph, 2, 3)));
+    directed_graph.add_edge(simple_graph::Edge<float>(3, 4, dist(directed_graph, 3, 4)));
+    directed_graph.add_edge(simple_graph::Edge<float>(4, 7, dist(directed_graph, 4, 7)));
+    directed_graph.add_edge(simple_graph::Edge<float>(0, 5, dist(directed_graph, 0, 5)));
+    directed_graph.add_edge(simple_graph::Edge<float>(5, 6, dist(directed_graph, 5, 6)));
+    directed_graph.add_edge(simple_graph::Edge<float>(6, 7, dist(directed_graph, 6, 7)));
+
+    std::vector<vertex_index_t> path;
+    std::function<float(vertex_index_t, vertex_index_t)> heuristic = [=](vertex_index_t c, vertex_index_t r) {
+        return dist(directed_graph, c, r);
+    };
+    EXPECT_EQ(true, astar(directed_graph, 0, 7, heuristic, &path));
+    EXPECT_EQ(6, path.size());
+    EXPECT_EQ(0, path[0]);
+    EXPECT_EQ(1, path[1]);
+    EXPECT_EQ(4, path[4]);
 }
 
 }  // namespace
